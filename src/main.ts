8917f22cad38f319ccf492164865e53459a690ab
konfortes <ronen.konfortes@riskified.com>
2021-02-11 17:04:01 +0200

    Add new relic

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,22 @@
-import { ConfigService } from '@nestjs/config';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import 'newrelic'
+import { ConfigService } from '@nestjs/config'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['debug', 'log', 'warn', 'error'],
-  });
+  })
 
-  const telegrafProvider = app.get('TelegrafProvider');
-  const configService = app.get(ConfigService);
+  const telegrafProvider = app.get('TelegrafProvider')
+  const configService = app.get(ConfigService)
 
   app.use(
     telegrafProvider.webhookCallback(
       configService.get('telegram.webhook.secretPath'),
     ),
-  );
+  )
 
-  await app.listen(configService.get('port'));
+  await app.listen(configService.get('port'))
 }
-bootstrap();
+bootstrap()
