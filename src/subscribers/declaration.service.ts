cdeb2984b63f36ca550cc15748472ac8316fd0d6
konfortes <ronen.konfortes@riskified.com>
2021-02-11 23:06:10 +0200

    Refactor DeclarationService to smaller functions

--- src/subscribers/declaration.service.ts
+++ src/subscribers/declaration.service.ts
@@ -6,6 +6,7 @@ import { Inject, Injectable } from '@nestjs/common'
 import { Logger } from 'winston'
 import { InjectBot, TelegrafProvider } from 'nestjs-telegraf'
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston'
+import { UserEntity } from 'src/data/entities/user.entity'
 
 @Injectable()
 export class DeclarationService {
@@ -19,9 +20,10 @@ export class DeclarationService {
   ) {
     this.logger = logger.child({ loggerName: DeclarationService.name })
   }
+
   async send() {
-    if (new Date().getDay() > 4) {
-      this.logger.info('not a week day. skipping')
+    if (!this.isSchoolDay()) {
+      this.logger.info('not a school day. skipping')
       return
     }
 
@@ -29,41 +31,18 @@ export class DeclarationService {
       // TODO: join links to avoid n+1 queries
       const subscribedUsers = await this.usersRepo.getSubscribedUsers()
 
-      // TODO: had some trouble running it in parallel on Heroku, that's why I changed it to run each user synchronously
+      // TODO: had some trouble running it in parallel on Heroku something with concurrent connections limitation, that's why I changed it to run each user synchronously
       for (const user of subscribedUsers) {
         try {
           const link = await this.linksRepo.getByUserId(user.id)
 
           const approvalUrl = await this.scraper.sendDeclaration(link.value)
 
-          if (process.env.MUTE_NOTIFICATIONS == 'true') {
-            this.logger.warn('Notifications are muted')
-            return
-          }
+          const msg = this.approvalMessage(link.name, approvalUrl)
 
-          const msg =
-            'שלחתי את הצהרת הבריאות של ' +
-            link.name +
-            '. ' +
-            'הנה האישור:' +
-            approvalUrl
-          await this.bot.telegram.sendMessage(user.external_id, msg)
+          await this.sendTelegramMessage(user.external_id, msg)
         } catch (ex) {
-          await this.adminNotification.notify(
-            `could not send declaration for user(${user.external_id} - ${
-              user.first_name
-            } ${user.last_name ? user.last_name : ''})`,
-          )
-
-          if (process.env.MUTE_NOTIFICATIONS == 'true') {
-            this.logger.warn('Notifications are muted')
-            return
-          }
-
-          await this.bot.telegram.sendMessage(
-            user.external_id,
-            'לא הצלחתי לשלוח את הצהרת הבריאות שלך היום.',
-          )
+          await this.handleSubscriptionError(user, ex)
         }
       }
     } catch (ex) {
@@ -72,5 +51,51 @@ export class DeclarationService {
         `error sending health declarations: ${ex}`,
       )
     }
+  }
+
+  private isSchoolDay() {
+    let schoolDay = true
+
+    if (new Date().getDay() > 4) {
+      schoolDay = false
+    }
+
+    return schoolDay
+  }
+
+  private async sendTelegramMessage(
+    userId: number | string,
+    text: string,
+  ): Promise<void> {
+    if (process.env.MUTE_NOTIFICATIONS == 'true') {
+      this.logger.warn('Notifications are muted')
+      return
+    }
+
+    await this.bot.telegram.sendMessage(userId, text)
+
+    return
+  }
+
+  private approvalMessage(name: string, approvalUrl: string): string {
+    const msg =
+      'שלחתי את הצהרת הבריאות של ' + name + '. ' + 'הנה האישור:' + approvalUrl
+    return msg
+  }
+
+  private async handleSubscriptionError(
+    user: UserEntity,
+    ex: Error,
+  ): Promise<void> {
+    await this.adminNotification.notify(
+      `could not send declaration for user(${user.external_id} - ${
+        user.first_name
+      } ${user.last_name ? user.last_name : ''}). ${ex}`,
+    )
+
+    await this.sendTelegramMessage(
+      user.external_id,
+      'לא הצלחתי לשלוח את הצהרת הבריאות שלך היום.',
+    )
   }
 }
